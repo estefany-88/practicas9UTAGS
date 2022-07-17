7bae89500d4619dfd808c876d570e40d1dc9f7c3
estefany-88 <80770386+estefany-88@users.noreply.github.com>
2022-07-16 19:30:54 -0500

    Update function1.py

--- function1.py
+++ function1.py
@@ -3,7 +3,7 @@ pwds="123d*"
 print("**Ingrese usuario y contraseña**")
 print("Usuario: ")
 user=input()
-print("Contraseña: ")
+print("Contraseña:  ")
 pwd1=input()
 if user==usuario and pwd1==pwds:
     print("Acceso correcto")
